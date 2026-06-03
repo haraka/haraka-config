5d15bd0ef3b95406e6ba3de499d0c6d5f5281165
Matt Simerson <matt@tnpi.net>
2026-06-02 22:23:27 -0700

    refactor: boost qlty and coverage

--- test/readers/binary.js
+++ test/readers/binary.js
@@ -1,4 +1,4 @@
-const assert = require('node:assert')
+const assert = require('node:assert/strict')
 const { beforeEach, describe, it } = require('node:test')
 const fs = require('node:fs')
 const path = require('node:path')
@@ -16,11 +16,24 @@ describe('binary', function () {
     assert.ok(typeof this.bin.load === 'function')
   })
 
+  it('has a loadPromise function', function () {
+    assert.ok(typeof this.bin.loadPromise === 'function')
+  })
+
   it('loads the test binary file', function () {
     const testBin = path.join('test', 'config', 'test.binary')
     const result = this.bin.load(testBin)
     assert.ok(Buffer.isBuffer(result))
     assert.equal(result.length, 120)
     assert.deepEqual(result, fs.readFileSync(testBin))
+  })
+
+  it('loads the test binary file with a promise', async function () {
+    const testBin = path.join('test', 'config', 'test.binary')
+    const result = await this.bin.loadPromise(testBin)
+    assert.equal(result.path, testBin)
+    assert.ok(Buffer.isBuffer(result.data))
+    assert.equal(result.data.length, 120)
+    assert.deepEqual(result.data, fs.readFileSync(testBin))
   })
 })
